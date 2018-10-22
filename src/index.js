1b4c251cf6012870db0c8ec26920925d10a30d7e
Juan Camilo Morales <camov1209@gmail.com>
2018-10-22 00:13:34 -0500

    Adds Redux arquitecture

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import { render } from 'react-dom'
+import thunkMiddleware from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import { createStore, applyMiddleware } from 'redux'
+import { rootReducer } from './reducers'
+import Root from './components/Root'
+import './index.scss';
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const loggerMiddleware = createLogger()
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware,
+    loggerMiddleware
+  )
+)
+
+render(
+  <Root store={store} />,
+  document.getElementById('root')
+)
+
+serviceWorker.register()
